959ea11c761e8c3cf29a5a597c79ef2f67a9de42
mabarber92 <mathew.barber.dh@gmail.com>
2024-08-12 14:12:12 +0100

    started evaluation update script

---
+++ citation-resolution/post_evaluation_update.py
@@ -0,0 +1,22 @@
+from create_evaluation_sheet import loop_through_ms, search_ms, merge_results_leveled
+import pandas as pd
+
+def create_update_uri_citation_map(evaluation_df, leveled_df_ids, uri_citation_map_path)
+
+
+
+
+def post_evaluation_update(evaluation_csv, uri_citation_map_path, leveled_df_ids, new_evaluation_round = True, text = None, new_evaluation_csv=None):
+
+    if not new_evaluation_round:
+        # Just update the uri_citation_map_path - in future run other functions to check or analyse data
+    
+    else:
+        # Run the processes that create a new evaluation_csv based on the old one
+
+ 
+
+if __name__ == "__main__":
+    evaluation_csv = ''
+    leveled_df = ''
+    text = ''
